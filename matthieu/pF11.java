1fae2a4333d58d14be683558cf68c8daa244e1cf
clorand <cedric.lorand@gmail.com>
2022-12-19 14:59:05 +0100

    subtract test

--- matthieu/pF11.java
+++ matthieu/pF11.java
@@ -22,7 +22,10 @@ public class pF11
 	
 	public pF11(int i)
 	{
-		this.value = new F11(i);
+		if ((i>=0)&&(i<11))
+			this.value = new F11(i);
+		else
+			this.value = INF;
 	}
 	
 	public pF11(String s)
@@ -54,17 +57,106 @@ public class pF11
 		return (this.value instanceof String)&&((String)this.value).equals(INF);
 	}
 	
+	public F11 toF11() throws Exception
+	{
+		F11 ret = null;
+		
+		if (this.isInf())
+		{
+			throw new Exception("Projective Field Value is infinite");
+		}
+		else
+		{
+			ret = ((F11)this.getValue());
+		}
+		
+		return ret;
+	}
+	
+	
+	public pF11 getInf()
+	{
+		return new pF11("INF");
+	}
+	
+	public  pF11 add(pF11 pf)
+	{
+		pF11 ret = null;
+		
+		try 
+		{
+			F11 f0 = this.toF11();
+			F11 f1 = pf.toF11();
+			ret = new pF11(f0.add(f1));
+		}
+			catch(Exception e)
+		{
+			ret = getInf();
+		}
+			
+		return ret;
+	}
+
+	public  pF11 subtract(pF11 pf)
+	{
+		pF11 ret = null;
+		
+		try 
+		{
+			F11 f0 = this.toF11();
+			F11 f1 = pf.toF11();
+			ret = new pF11(f0.subtract(f1));
+		}
+			catch(Exception e)
+		{
+			ret = getInf();
+		}
+			
+		return ret;
+	}
+
+	public  pF11 multiply(pF11 f)
+	{
+		return null;
+	}
+	
+	public  pF11 divide(pF11 f)
+	{
+		return null;
+	}
+	
 	public static void main(String[] args)
 	{
-		for (int i=0; i<11; i++)
+		for (int i=0; i<12; i++)
 		{
 				pF11 f = new pF11(i);
 				System.out.println(""+f+", "+f.isInf());
 		}
 		
-		pF11 f = new pF11("inf");
-		System.out.println(""+f+", "+f.isInf());
+		for (int i=0; i<12; i++)
+		{
+			for (int j=0; j<12; j++)
+			{
+				pF11 fi = new pF11(i);
+				pF11 fj = new pF11(j);
+				System.out.print(" "+fi.add(fj));
+			}
+			System.out.println("");
+		}
+
+		System.out.println("");
+		
+		for (int i=0; i<12; i++)
+		{
+			for (int j=0; j<12; j++)
+			{
+				pF11 fi = new pF11(i);
+				pF11 fj = new pF11(j);
+				System.out.print(" "+fi.subtract(fj));
+			}
+			System.out.println("");
+		}
+		
 		
 	}
-
 }
